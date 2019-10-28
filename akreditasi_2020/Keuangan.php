a423065107d93b7a22bedcfc7b4548ec987c1efd
Alfaza Ganz <55083326+vosetrick@users.noreply.github.com>
2019-10-28 11:17:11 +0700

    Add files via upload

---
+++ akreditasi_2020/Keuangan.php
@@ -0,0 +1,54 @@
+<head>
+        <title>Keuangan</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body style="height: 100%">
+        <div class="judul-block">
+            <div class="judul-kontainer">
+                <div>
+                    <img class="unpar-side" src="img/unpar.png" alt="">
+                </div>
+                <div>
+                    <h2 style="padding: 10px;">Keuangan, Sarana dan Prasarana</h2>
+                </div>
+                <div>
+                    <img class="if-side" src="img/logoIF.jpg" alt="">
+                </div>
+            </div>
+        </div>
+        <div>
+            <ul>
+                <div style="float: left">
+                    <?php
+                        include("Sidebar.php");
+                    ?>
+                </div>  
+                <div>
+                    <!--Masukan id div konten kalian sesuai dengan format dibawah-->
+                    <li><a href="#konten-1" onclick="show('konten-1')">konten 1</a></li>
+                    <li><a href="#konten-2" onclick="show('konten-2')">konten 2</a></li>
+                </div>
+            </ul>
+        </div>
+        <!--Konten Diisi di div dibawah sini, jangan lupa tambahkan kelas "konten" dan idnya masing-masing--> 
+        <div class="konten" id="konten-1">
+            asd
+        </div>
+        <div class="konten" id="konten-2">
+            dsa
+        </div>
+        <script>
+            close();
+            function close() {
+                var x = document.getElementsByClassName("konten");
+                for (var i = 0; i < x.length; i++) {
+                    x[i].style.display = "none";
+                }
+            }
+            function show(id) {
+                close();
+                document.getElementById(id).style.display = "block";
+            }
+        </script>
+</body>
