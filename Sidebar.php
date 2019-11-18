936e1326dc3cbb77b2ead482ba5fd1fe8f2c2136
Chris Eldon <eldon922@gmail.com>
2019-11-18 22:32:02 +0700

    Rounded corner button and improve sidebar

--- Sidebar.php
+++ Sidebar.php
@@ -17,48 +17,48 @@
                     <a class="link-konten-slide" href="TataPamong.php">
                         <div class="bar-flex-side">
                             <img class="gambar-konten-side" src="img/kerjasama.png">
-                        </div>
+                        </div> Kerja sama
                     </a>
                     <a class="link-konten-slide" href="Mahasiswa.php">
                         <div class="bar-flex-side">
                             <img class="gambar-konten-side" src="img/mahasiswa.png">
-                        </div>
+                        </div> Mahasiswa
                     </a>
                     <a class="link-konten-slide" href="SDM.php">
                         <div class="bar-flex-side">
                             <img class="gambar-konten-side" src="img/sdm.png">
-                        </div>
+                        </div> Sumber Daya Manusia
                     </a>
                     <a class="link-konten-slide" href="Keuangan.php">
                         <div class="bar-flex-side">
                             <img class="gambar-konten-side" src="img/keuangan.png">
-                        </div>
+                        </div> Keuangan
                     </a>        
                     <a class="link-konten-slide" href="Pendidikan.php">
                         <div class="bar-flex-side">
                             <img class="gambar-konten-side" src="img/akademik.png">
-                        </div>
+                        </div> Akademik
                     </a>
                     <a class="link-konten-slide" href="Penelitian.php">
                         <div class="bar-flex-side">
                             <img class="gambar-konten-side" src="img/penelitian.png">
-                        </div>
+                        </div> Penelitian
                     </a>
                     <a class="link-konten-slide" href="Pengabdian.php">
                         <div class="bar-flex-side">
                             <img class="gambar-konten-side" src="img/pengmas.png">
-                        </div>
+                        </div> Pengabdian Masyarakat
                     </a>
                     <a class="link-konten-slide" href="Capaian.php">
                         <div class="bar-flex-side">
                             <img class="gambar-konten-side" src="img/pencapaian.png">
-                        </div>
+                        </div> Pencapaian
                     </a>
         </div>
 </div>
         <script>
                 function openNav() {
-                    document.getElementById("mySidebar").style.width = "90px";
+                    document.getElementById("mySidebar").style.width = "30%";
                     document.getElementById("main").style.marginLeft = "0px";
                 }
             
