aacbf9c70013da1a21f486e0c3924554d7ac68d7
Chris Eldon <eldon922@gmail.com>
2019-11-18 22:45:47 +0700

    fix sidebar

--- Sidebar.php
+++ Sidebar.php
@@ -16,49 +16,49 @@
                 <a id="home" class="link-konten-slide" href="Homepage.php">HOME</a>
                     <a class="link-konten-slide" href="TataPamong.php">
                         <div class="bar-flex-side">
-                            <img class="gambar-konten-side" src="img/kerjasama.png">
-                        </div> Kerja sama
+                            <img class="gambar-konten-side" src="img/kerjasama.png"> <span class="labelMenu">Kerja sama</span>
+                        </div>
                     </a>
                     <a class="link-konten-slide" href="Mahasiswa.php">
                         <div class="bar-flex-side">
-                            <img class="gambar-konten-side" src="img/mahasiswa.png">
-                        </div> Mahasiswa
+                            <img class="gambar-konten-side" src="img/mahasiswa.png"> <span class="labelMenu">Mahasiswa</span>
+                        </div>
                     </a>
                     <a class="link-konten-slide" href="SDM.php">
                         <div class="bar-flex-side">
-                            <img class="gambar-konten-side" src="img/sdm.png">
-                        </div> Sumber Daya Manusia
+                            <img class="gambar-konten-side" src="img/sdm.png"> <span class="labelMenu">Sumber Daya Manusia</span>
+                        </div>
                     </a>
                     <a class="link-konten-slide" href="Keuangan.php">
                         <div class="bar-flex-side">
-                            <img class="gambar-konten-side" src="img/keuangan.png">
-                        </div> Keuangan
+                            <img class="gambar-konten-side" src="img/keuangan.png"> <span class="labelMenu">Keuangan</span>
+                        </div>
                     </a>        
                     <a class="link-konten-slide" href="Pendidikan.php">
                         <div class="bar-flex-side">
-                            <img class="gambar-konten-side" src="img/akademik.png">
-                        </div> Akademik
+                            <img class="gambar-konten-side" src="img/akademik.png"> <span class="labelMenu">Akademik</span>
+                        </div>
                     </a>
                     <a class="link-konten-slide" href="Penelitian.php">
                         <div class="bar-flex-side">
-                            <img class="gambar-konten-side" src="img/penelitian.png">
-                        </div> Penelitian
+                            <img class="gambar-konten-side" src="img/penelitian.png"> <span class="labelMenu">Penelitian</span>
+                        </div>
                     </a>
                     <a class="link-konten-slide" href="Pengabdian.php">
                         <div class="bar-flex-side">
-                            <img class="gambar-konten-side" src="img/pengmas.png">
-                        </div> Pengabdian Masyarakat
+                            <img class="gambar-konten-side" src="img/pengmas.png"> <span class="labelMenu">Pengabdian Masyarakat</span>
+                        </div>
                     </a>
                     <a class="link-konten-slide" href="Capaian.php">
                         <div class="bar-flex-side">
-                            <img class="gambar-konten-side" src="img/pencapaian.png">
-                        </div> Pencapaian
+                            <img class="gambar-konten-side" src="img/pencapaian.png"> <span class="labelMenu">Pencapaian</span>
+                        </div>
                     </a>
         </div>
 </div>
         <script>
                 function openNav() {
-                    document.getElementById("mySidebar").style.width = "30%";
+                    document.getElementById("mySidebar").style.width = "25%";
                     document.getElementById("main").style.marginLeft = "0px";
                 }
             
